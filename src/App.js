61c6bfbee055fbf4de1c44b152e97a6e0f098224
ozaki25 <fs65ugumori@yahoo.co.jp>
2020-03-12 13:54:19 +0900

    add react component

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { gql } from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
+
+import client from './graphql/client';
+
+client
+  .query({
+    query: gql`
+      {
+        rates(currency: "USD") {
+          currency
+        }
+      }
+    `,
+  })
+  .then(result => console.log(result));
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <div>
+        <h2>My first Apollo app 🚀</h2>
+      </div>
+    </ApolloProvider>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,6 @@
-import ApolloClient, { gql } from 'apollo-boost';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-const client = new ApolloClient({
-  uri: 'https://48p1r2roz4.sse.codesandbox.io',
-});
+import App from './App';
 
-client
-  .query({
-    query: gql`
-      {
-        rates(currency: "USD") {
-          currency
-        }
-      }
-    `,
-  })
-  .then(result => console.log(result));
+ReactDOM.render(<App />, document.getElementById('root'));
